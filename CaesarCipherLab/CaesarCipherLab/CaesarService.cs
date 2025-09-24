cd959b37435ba200549024758efa40ab12c6b7bf
Alexandra wintle <awintle@hotmail.com>
2025-09-24 15:16:15 -0400

    Add files via upload

---
+++ CaesarCipherLab/CaesarCipherLab/CaesarService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CaesarCipherLab
+{
+    public static class CaesarService
+    {
+        public static ProcessResult Process(string trainingPath, string scrambledPath, string outputPath)
+        {
+            string trainingText = File.ReadAllText(trainingPath);
+            int lineCount = File.ReadLines(trainingPath).Count();
+            int charCount = trainingText.Length;
+
+            var trainingFreq = CountLetters(trainingText);
+            var trainingTop = trainingFreq.OrderByDescending(x => x.Value).Take(2).ToList();
+
+            string scrambledText = File.ReadAllText(scrambledPath);
+            var scrambledFreq = CountLetters(scrambledText);
+            var scrambledTop = scrambledFreq.OrderByDescending(x => x.Value).First();
+
+            int shift = GetShift(trainingTop[0].Key, scrambledTop.Key);
+            string decrypted = ShiftText(scrambledText, -shift);
+            File.WriteAllText(outputPath, decrypted);
+
+            return new ProcessResult
+            {
+                LineCount = lineCount,
+                CharCount = charCount,
+                TrainingTop1 = trainingTop[0],
+                TrainingTop2 = trainingTop[1],
+                ScrambledTop = scrambledTop,
+                Shift = shift
+            };
+        }
+
+        private static Dictionary<char, int> CountLetters(string text)
+        {
+            var counts = new Dictionary<char, int>();
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char upper = char.ToUpper(c);
+                    if (!counts.ContainsKey(upper)) counts[upper] = 0;
+                    counts[upper]++;
+                }
+            }
+            return counts;
+        }
+
+        private static int GetShift(char trainingLetter, char scrambledLetter)
+        {
+            int t = trainingLetter - 'A';
+            int s = scrambledLetter - 'A';
+            return (s - t + 26) % 26;
+        }
+
+        private static string ShiftText(string input, int shift)
+        {
+            string result = "";
+            foreach (char c in input)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    result += (char)('A' + (c - 'A' + shift + 26) % 26);
+                }
+                else if (c >= 'a' && c <= 'z')
+                {
+                    result += (char)('a' + (c - 'a' + shift + 26) % 26);
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+    }
+
+    public class ProcessResult
+    {
+        public int LineCount { get; set; }
+        public int CharCount { get; set; }
+        public KeyValuePair<char, int> TrainingTop1 { get; set; }
+        public KeyValuePair<char, int> TrainingTop2 { get; set; }
+        public KeyValuePair<char, int> ScrambledTop { get; set; }
+        public int Shift { get; set; }
+    }
+}
---
+++ CaesarCipherLab/CaesarCipherLab/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace CaesarCipherLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Usage: CaesarCipherLab <trainingFile> <scrambledFile> <outputFile>");
+                    Console.ReadLine();
+                    return;
+                }
+
+                string trainingPath = args[0];
+                string scrambledPath = args[1];
+                string outputPath = args[2];
+
+                var result = CaesarService.Process(trainingPath, scrambledPath, outputPath);
+
+                Console.WriteLine("1. Reading input file \"" + Path.GetFileName(trainingPath) + "\".");
+                Console.WriteLine("2. Length of input file is " + result.LineCount + " lines, and " + result.CharCount + " characters.");
+                Console.WriteLine("3. The two most occurring characters are " + result.TrainingTop1.Key + " and " + result.TrainingTop2.Key +
+                                  ", occurring " + result.TrainingTop1.Value + " times and " + result.TrainingTop2.Value + " times.");
+                Console.WriteLine("4. Reading the encrypted file \"" + Path.GetFileName(scrambledPath) + "\".");
+                Console.WriteLine("5. The most occurring character is " + result.ScrambledTop.Key +
+                                  ", occurring " + result.ScrambledTop.Value + " times.");
+                Console.WriteLine("6. A shift factor of " + result.Shift + " has been determined.");
+                Console.WriteLine("7. Writing output file now to \"" + Path.GetFileName(outputPath) + "\".");
+
+                // file read
+                Console.Write("8. Display the file? (y/n): ");
+                string choice = Console.ReadLine();
+
+                if (choice != null && choice.ToLower() == "y")
+                {
+                    string[] lines = File.ReadAllLines(outputPath);
+                    Console.WriteLine("\n--- Output File Contents ---");
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    Console.WriteLine("--- End of File ---\n");
+                }
+
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
